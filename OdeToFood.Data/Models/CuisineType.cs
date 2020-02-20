192bf04722225323d57440f4741f11d81e02ecd7
Vincent Oblak <vincent.oblak@ticomix.com>
2020-02-20 16:51:41 -0600

    Created ASP.NET project with initial files

---
+++ OdeToFood.Data/Models/CuisineType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Models
+{
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        Indian,
+        French
+    }
+}
---
+++ OdeToFood.Data/Services/IRestaurantData.cs
@@ -0,0 +1,11 @@
+﻿using OdeToFood.Data.Models;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+}
